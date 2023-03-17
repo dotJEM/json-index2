ade41ac3612d1bb31e82045beca914e52bea3c4f
jmd <Jens.Melgaard@Systematic.com>
2023-03-17 12:20:51 +0100

    spike

---
+++ src/DotJEM.Json.Index2/Class1.cs
@@ -0,0 +1,47 @@
+﻿using Lucene.Net.Analysis;
+using Lucene.Net.Util;
+
+namespace DotJEM.Json.Index2;
+public interface IJsonIndex
+{
+    //IInfoStream InfoStream { get; }
+
+
+    IServiceCollection Services { get; }
+    IJsonIndexStorage Storage { get; }
+    IJsonIndexConfiguration Configuration { get; }
+}
+
+public interface IJsonIndexConfiguration
+{
+    LuceneVersion Version { get; }
+    Analyzer Analyzer { get; }
+}
+
+public interface IJsonIndexStorage
+{
+}
+
+public class LuceneJsonIndex : IJsonIndex
+{
+    //public IInfoStream InfoStream { get; }
+    public IServiceCollection Services { get; }
+    public IJsonIndexStorage Storage { get; }
+    public IJsonIndexConfiguration Configuration { get; }
+
+    public LuceneJsonIndex() { }
+    public LuceneJsonIndex(string path) { }
+
+    public LuceneJsonIndex(IJsonIndexStorage storage, IJsonIndexConfiguration configuration, IServiceCollection services)
+    {
+
+    }
+
+
+
+}
+
+public interface IServiceCollection
+{
+
+}
