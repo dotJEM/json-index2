0d4b952850080a117e236a0252805e9e250ba2db
Jens Melgaard <admin@it-links.dk>
2024-02-21 20:06:21 +0100

    added extra helping interface to change events.

--- src/DotJEM.Json.Index2.Management/Source/IJsonDocumentSourceEvent.cs
+++ src/DotJEM.Json.Index2.Management/Source/IJsonDocumentSourceEvent.cs
@@ -8,16 +8,18 @@ public interface IJsonDocumentSourceEvent
     string Area { get; }
 }
 
-//public interface IJsonDocumentSourceChange
-//{
-//    string Area { get; }
-//}
+public interface IJsonDocumentChangeEvent : IJsonDocumentSourceEvent
+{
+    int Size { get; }
+    JObject Document { get; }
+    GenerationInfo Generation { get; }
+}
 
-public readonly record struct JsonDocumentCreated(string Area, JObject Document, int Size, GenerationInfo Generation) : IJsonDocumentSourceEvent;
+public readonly record struct JsonDocumentCreated(string Area, JObject Document, int Size, GenerationInfo Generation) : IJsonDocumentChangeEvent;
 
-public readonly record struct JsonDocumentUpdated(string Area, JObject Document, int Size, GenerationInfo Generation): IJsonDocumentSourceEvent;
+public readonly record struct JsonDocumentUpdated(string Area, JObject Document, int Size, GenerationInfo Generation): IJsonDocumentChangeEvent;
 
-public readonly record struct JsonDocumentDeleted(string Area, JObject Document, int Size, GenerationInfo Generation): IJsonDocumentSourceEvent;
+public readonly record struct JsonDocumentDeleted(string Area, JObject Document, int Size, GenerationInfo Generation): IJsonDocumentChangeEvent;
 
 public readonly record struct JsonDocumentSourceDigestCompleted(string Area) : IJsonDocumentSourceEvent;
 public readonly record struct JsonDocumentSourceReset(string Area) : IJsonDocumentSourceEvent;
--- src/DotJEM.Json.Index2.Management/Source/JsonDocumentSourceChange.cs
+++
@@ -1,5 +0,0 @@
-﻿using System;
-using Newtonsoft.Json.Linq;
-
-namespace DotJEM.Json.Index2.Management.Source;
-
